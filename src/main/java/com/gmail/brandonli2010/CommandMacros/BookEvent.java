8a99a54a5771d8e98923b38938dca5c77c10d4a8
thepowderguy <brandonli2010@gmail.com>
2014-02-18 11:06:06 -0500

    Init, eventhandler

---
+++ src/main/java/com/gmail/brandonli2010/CommandMacros/BookEvent.java
@@ -0,0 +1,59 @@
+package com.gmail.brandonli2010.CommandMacros;
+
+import java.util.logging.Logger;
+
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.meta.BookMeta;
+
+public class BookEvent implements Listener {
+	private final CommandMacros plugin;
+	public BookEvent(CommandMacros instance) {
+        plugin = instance;
+    }
+	@EventHandler
+	public void onPlayerInteract(PlayerInteractEvent event)
+	{
+		if ((event.getAction() == Action.LEFT_CLICK_AIR) | (event.getAction() == Action.LEFT_CLICK_BLOCK))
+		{
+			if (event.getItem() != null)
+			{
+				if (event.getItem().getType() == Material.WRITTEN_BOOK)
+				{
+					 BookMeta thisbook = (BookMeta) event.getItem().getItemMeta();
+					 if (thisbook.getTitle().equals("command"))
+					 {
+						 if (!event.getPlayer().hasPermission("commandmacros.macro"))
+						 {
+							 event.getPlayer().sendMessage("You dont have permissions to use a macro.");
+							 return;
+						 }
+						 if ((thisbook.getPageCount() > plugin.getConfig().getInt("maxcommands")) & (!(plugin.getConfig().getInt("maxcommands") == 0)))
+						 {
+							 event.getPlayer().sendMessage("there cannot be more than " + "commannds on a book.");
+							 return;
+						 }
+						 Logger cmdlog = plugin.getLogger();
+						 String pname = event.getPlayer().getName();
+						 for (int ind = 1; ind <= thisbook.getPageCount(); ind ++)
+						 {
+							 if (thisbook.getPage(ind).startsWith("/") & plugin.getConfig().getBoolean("commandsenabled"))
+							 {
+								 event.getPlayer().chat(thisbook.getPage(ind));
+								 cmdlog.info(pname + " issued server command " + thisbook.getPage(ind));
+							 }
+							 else if (plugin.getConfig().getBoolean("chatenabled"))
+							 {
+								 event.getPlayer().chat(thisbook.getPage(ind));
+							 }
+						 }
+						 event.setCancelled(true);
+					 }
+				}
+			}
+		}
+	}
+}
