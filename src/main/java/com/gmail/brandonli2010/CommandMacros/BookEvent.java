90b2b4f1aa1a433ceb7eed25d0516864f13a0502
thepowderguy <brandonli2010@gmail.com>
2014-02-22 15:13:36 -0500

    Update BookEvent.java

--- src/main/java/com/gmail/brandonli2010/CommandMacros/BookEvent.java
+++ src/main/java/com/gmail/brandonli2010/CommandMacros/BookEvent.java
@@ -41,4 +41,49 @@ public class BookEvent implements Listener {
 			}
 		}
 	}
+
+	@EventHandler
+	public void onBlockDispense(BlockDispenseEvent event)
+	{
+		if (!plugin.getConfig().getBoolean("enabledispensers"))
+		{
+			return;
+		}
+		if (event.getItem() != null)
+		{
+			if (event.getItem().getType() == Material.WRITTEN_BOOK)
+			{
+				BookMeta thisbook = (BookMeta) event.getItem().getItemMeta();
+				if ((thisbook.getTitle() != null) && thisbook.getTitle().equals(plugin.getConfig().getString("bookname")))
+				{
+					if (thisbook.getAuthor().equals("@console"))
+					{
+						if (plugin.getConfig().getBoolean("doconsolebooks"))
+						{
+							event.setCancelled(true);
+							plugin.executeconsolebook(thisbook);
+						}
+						return;
+					}
+					Player sender = Bukkit.getPlayer(thisbook.getAuthor());
+					if (sender == null)
+					{
+						return;
+					}
+					if (!sender.hasPermission("commandmacros.macro"))
+					{
+						sender.sendMessage("\u00a7cYou dont have permissions to use a macro.");
+						return;
+					}
+					if ((thisbook.getPageCount() > plugin.getConfig().getInt("maxcommands")) & (!(plugin.getConfig().getInt("maxcommands") == 0)))
+					{
+						sender.sendMessage("\u00a7cThere cannot be more than " + plugin.getConfig().getInt("maxcommands") + " commands in a book.");
+						return;
+					}
+					event.setCancelled(true);
+					plugin.executeBook(sender, thisbook);
+				}
+			}
+		}
+	}
 }
